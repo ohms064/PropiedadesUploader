48dd80b88d58e8f666c63a17b71eb139231220bb
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-06-02 00:00:55 -0500

    Ventana del sistema.

---
+++ PropiedadesUploader/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropiedadesUploader {
+    public partial class Form1 : Form {
+        OpenFileDialog ofdImagePreview = new OpenFileDialog();
+        OpenFileDialog ofdDescriptionImages = new OpenFileDialog();
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click( object sender, EventArgs e ) {
+            if ( ofdImagePreview.ShowDialog() == DialogResult.OK ) {
+                imagen1TextBox.Text = ofdImagePreview.SafeFileName;
+                
+            }
+        }
+
+        private void button2_Click( object sender, EventArgs e ) {
+            ofdDescriptionImages.Multiselect = true;
+            if ( ofdDescriptionImages.ShowDialog() == DialogResult.OK ) {
+                descriptionImagesNames.Text = "";
+                foreach ( String file in ofdDescriptionImages.SafeFileNames ) {
+                    descriptionImagesNames.Text += file; 
+                    
+                }
+            }
+        }
+    }
+}
